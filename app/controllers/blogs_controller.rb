a2b7fbd66f4bc717eff666847842b3f21c8ad255
hyuna-hub <61468181+hyuna-hub@users.noreply.github.com>
2020-03-11 15:37:32 +0900

    [Start]4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,8 +5,13 @@ class BlogsController < ApplicationController
   def show
   end
 
+  def index
+    	# 記事を全件取得
+    	@blogs = Blog.all
+  end
+
   def new
-  	  @blog = Blog.new
+  	    @blog = Blog.new
   end
 
   def create
